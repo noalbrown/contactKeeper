731c85f62e5b338d7d36e74df9e5bcbaffb171d3
Noal Brown <noalbrown1@gmail.com>
2020-12-09 13:44:42 -0600

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config')
+const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 const User = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.send('Get logged in user')
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+
+    res.json(user)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
 });
 
 router.post('/', [
